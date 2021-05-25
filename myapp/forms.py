cd46dc5c82f4f00eb25142381b7f62945e03a5cd
dor-polo <dorpolo@gmail.com>
2021-05-25 18:46:03 +0300

    add migrations

--- myapp/forms.py
+++ myapp/forms.py
@@ -5,49 +5,82 @@ from .models import Game, League, LeagueUser
 class BetForm(forms.ModelForm):
     class Meta:
         model = Game
-        fields = ('user_name', 'gid_8222', 'gid_8198', 'gid_8199', 'gid_8200', 'gid_8201', 'gid_19950',
-                  'gid_8202', 'gid_19951', 'gid_19952', 'gid_8203', 'gid_19949', 'gid_8204',
-                  'gid_8205', 'gid_8206', 'gid_8207', 'gid_19953', 'gid_8208', 'gid_8209',
-                  'gid_19954', 'gid_8210', 'gid_19955', 'gid_19956', 'gid_8211', 'gid_8212',
-                  'gid_8213', 'gid_8214', 'gid_8215', 'gid_19957', 'gid_8216', 'gid_8217',
-                  'gid_8218', 'gid_19958', 'gid_8219', 'gid_19959', 'gid_8220', 'gid_19960')
+        fields = (
+            'user_name',
+            'gid_8222',
+            'gid_8198',
+            'gid_8206',
+            'gid_8207',
+            'gid_8213',
+            'gid_8214',
+            'gid_8199',
+            'gid_8200',
+            'gid_8205',
+            'gid_8208',
+            'gid_8216',
+            'gid_8217',
+            'gid_19950',
+            'gid_8202',
+            'gid_19953',
+            'gid_8209',
+            'gid_8215',
+            'gid_19957',
+            'gid_8201',
+            'gid_19951',
+            'gid_8210',
+            'gid_19955',
+            'gid_8218',
+            'gid_19958',
+            'gid_19952',
+            'gid_8203',
+            'gid_19954',
+            'gid_8212',
+            'gid_8219',
+            'gid_19959',
+            'gid_19949',
+            'gid_8204',
+            'gid_19956',
+            'gid_8211',
+            'gid_8220',
+            'gid_19960',
+        )
 
         widgets = {
-            'user_id': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'e.g: Avi Nimni'}),
+            'user_name': forms.Select(attrs={'class': 'form-select', 'placeholder': 'e.g: Avi Nimni'}),
             'gid_8222': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'e.g: 0-0'}),
             'gid_8198': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'e.g: 0-0'}),
-            'gid_8199': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'e.g: 0-0'}),
-            'gid_8200': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'e.g: 0-0'}),
-            'gid_8201': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'e.g: 0-0'}),
-            'gid_19950': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'e.g: 0-0'}),
-            'gid_8202': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'e.g: 0-0'}),
-            'gid_19951': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'e.g: 0-0'}),
-            'gid_19952': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'e.g: 0-0'}),
-            'gid_8203': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'e.g: 0-0'}),
-            'gid_19949': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'e.g: 0-0'}),
-            'gid_8204': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'e.g: 0-0'}),
-            'gid_8205': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'e.g: 0-0'}),
             'gid_8206': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'e.g: 0-0'}),
             'gid_8207': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'e.g: 0-0'}),
-            'gid_19953': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'e.g: 0-0'}),
-            'gid_8208': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'e.g: 0-0'}),
-            'gid_8209': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'e.g: 0-0'}),
-            'gid_19954': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'e.g: 0-0'}),
-            'gid_8210': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'e.g: 0-0'}),
-            'gid_19955': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'e.g: 0-0'}),
-            'gid_19956': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'e.g: 0-0'}),
-            'gid_8211': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'e.g: 0-0'}),
-            'gid_8212': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'e.g: 0-0'}),
             'gid_8213': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'e.g: 0-0'}),
             'gid_8214': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'e.g: 0-0'}),
-            'gid_8215': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'e.g: 0-0'}),
-            'gid_19957': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'e.g: 0-0'}),
+            'gid_8199': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'e.g: 0-0'}),
+            'gid_8200': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'e.g: 0-0'}),
+            'gid_8205': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'e.g: 0-0'}),
+            'gid_8208': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'e.g: 0-0'}),
             'gid_8216': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'e.g: 0-0'}),
             'gid_8217': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'e.g: 0-0'}),
+            'gid_19950': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'e.g: 0-0'}),
+            'gid_8202': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'e.g: 0-0'}),
+            'gid_19953': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'e.g: 0-0'}),
+            'gid_8209': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'e.g: 0-0'}),
+            'gid_8215': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'e.g: 0-0'}),
+            'gid_19957': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'e.g: 0-0'}),
+            'gid_8201': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'e.g: 0-0'}),
+            'gid_19951': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'e.g: 0-0'}),
+            'gid_8210': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'e.g: 0-0'}),
+            'gid_19955': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'e.g: 0-0'}),
             'gid_8218': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'e.g: 0-0'}),
             'gid_19958': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'e.g: 0-0'}),
+            'gid_19952': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'e.g: 0-0'}),
+            'gid_8203': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'e.g: 0-0'}),
+            'gid_19954': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'e.g: 0-0'}),
+            'gid_8212': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'e.g: 0-0'}),
             'gid_8219': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'e.g: 0-0'}),
             'gid_19959': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'e.g: 0-0'}),
+            'gid_19949': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'e.g: 0-0'}),
+            'gid_8204': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'e.g: 0-0'}),
+            'gid_19956': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'e.g: 0-0'}),
+            'gid_8211': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'e.g: 0-0'}),
             'gid_8220': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'e.g: 0-0'}),
             'gid_19960': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'e.g: 0-0'}),
         }
@@ -66,23 +99,13 @@ class LeagueForm(forms.ModelForm):
 class UserForm(forms.ModelForm):
     class Meta:
         model = LeagueUser
-        fields = ('first_name', 'last_name', 'email', )
+        fields = ('user_name', 'league_name', 'first_name', 'last_name', 'email', )
 
         widgets = {
+            'user_name': forms.Select(attrs={'class': 'form-select', 'placeholder': 'e.g: Avi Nimni'}),
+            'league_name': forms.Select(attrs={'class': 'form-select', 'placeholder': 'e.g: Avi Nimni'}),
             'first_name': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'e.g: Avi'}),
             'last_name': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'e.g: Nimni'}),
             'email': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'e.g: tal_banin@gmail.com'}),
         }
 
-    def clean_name(self):
-        phone = self.cleaned_data.get("first_name")
-        if len(phone) != 2:
-            raise forms.ValidationError("Invalid phone number")
-        return phone
-
-    def clean(self):
-        first_name = self.cleaned_data.get("first_name")
-        last_name = self.cleaned_data.get("first_name")
-        # Custom Validation Logic
-        # Here
-        return self.cleaned_data
--- pipelines/read_data.py
+++ pipelines/read_data.py
@@ -3,7 +3,7 @@ import json
 import environ
 import os
 from pathlib import Path
-from data.teams import teams
+from data.teams import teams, groups
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 env = environ.Env(SECRET_KEY=str,)
@@ -25,22 +25,30 @@ class EuroApi:
         data = self.extract_data()
         fixtures_list = []
         for item in data['calendar']['matchdays']:
-            for key, value in item.items():
-                if key == 'matches':
-                    for item2 in value:
-                        game_id = item2['matchID']
-                        home_team_id = item2['homeParticipant']['participantID']
-                        away_team_id = item2['awayParticipant']['participantID']
-                        home_team = item2['homeParticipant']['participantName']
-                        away_team = item2['awayParticipant']['participantName']
-                        home_logo = teams[home_team]['logo']
-                        away_logo = teams[away_team]['logo']
-                        home_score = item2['homeParticipant']['score']
-                        away_score = item2['awayParticipant']['score']
-                        row = [home_team, home_logo, away_team, away_logo, home_score, away_score]
-                        fixtures_list.append(row)
+            for sub_item in item['matches']:
+                game_id = sub_item['matchID']
+                home_team_id = sub_item['homeParticipant']['participantID']
+                away_team_id = sub_item['awayParticipant']['participantID']
+                home_team = sub_item['homeParticipant']['participantName']
+                away_team = sub_item['awayParticipant']['participantName']
+                home_logo = teams[home_team]['logo']
+                away_logo = teams[away_team]['logo']
+                home_score = sub_item['homeParticipant']['score']
+                away_score = sub_item['awayParticipant']['score']
+                group = groups[home_team]['groupName']
+                matchdayID = item['matchdayID']
+                match_day = item['matchdayName']
+                match_date = sub_item['matchDate']
+                match_time = sub_item['matchTime']
+                row = [home_team, home_logo, away_team, away_logo, home_score, away_score, match_day, match_date, match_time]
+                fixtures_list.append(row)
         return fixtures_list
+
+    @staticmethod
+    def dynamic_sort(sub_li):
+        sub_li.sort(key = lambda x: [x[7], x[8]])
+        return sub_li
 
     def main(self):
         output = self.read_fixtures()
-        return output
+        return self.dynamic_sort(output)
