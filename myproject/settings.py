30cc306255de3dee5aa3f1a129ea53b82c23c382
dor-polo <dorpolo@gmail.com>
2021-06-02 16:19:11 +0300

    Add nickname

--- myproject/settings.py
+++ myproject/settings.py
@@ -127,8 +127,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+MEDIA_URL = '/images/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'images/')
 STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL = 'home'
@@ -137,12 +137,10 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'dorpolo@gmail.com'
-EMAIL_HOST_PASSWORD = 'dorpolo444'
+EMAIL_HOST_USER = env('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = env('GMAIL_PASS')
 EMAIL_USE_TLS = True
-EMAIL_USE_SSL = True
-
-
+EMAIL_USE_SSL = False
 
 
 django_heroku.settings(locals())
